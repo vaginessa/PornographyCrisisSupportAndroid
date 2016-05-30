35c64191c4b732bc0c6197f05b50769004011a12
Stephen Nielson <stephen@nielson.org>
2016-05-30 15:07:41 -0600

    Merge branch 'master' of https://github.com/adunsulag/PornographyCrisisSupportAndroid
    Conflicts:
    	app/src/main/java/com/discoverandchange/pornographycrisissupport/Quiz.java

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/Quiz.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/Quiz.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- * Quiz R Us is what this does.
+ * This should conflict.
  * This class will hopefully, eventually, do something useful
  */
 

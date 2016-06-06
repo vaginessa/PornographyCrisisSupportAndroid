c21c54149ad7acb8619409b2580faadfc7b50b61
Higbee-Keith <hig12012@byui.edu>
2016-06-06 15:46:16 -0700

    Quiz package

---
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/Quiz.java
@@ -0,0 +1,19 @@
+package com.discoverandchange.pornographycrisissupport.quiz;
+
+/**
+ *
+ */
+
+public class Quiz {
+
+    private int score;
+
+    public Quiz(int score)
+    {
+        this.score = score;
+    }
+
+    public Quiz() {
+        this(0);
+    }
+}
---
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/QuizService.java
@@ -0,0 +1,19 @@
+package com.discoverandchange.pornographycrisissupport.quiz;
+
+import java.util.List;
+
+/**
+ *
+ */
+
+public class QuizService {
+    List<Quiz> listScores;
+
+    public boolean saveQuiz(Quiz quiz) {
+        return false;
+    }
+
+    public int getLatestQuizScore() {
+        return 0;
+    }
+}

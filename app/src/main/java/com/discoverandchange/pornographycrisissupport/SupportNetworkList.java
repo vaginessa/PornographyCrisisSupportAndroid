858a7a0675c297070397db2660ed939a2fecef8e
John Okleberry <okl13001@byui.edu>
2016-06-08 16:28:17 -0600

    2016-06-08 Peer Program John End (partial contact info pulling)

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/SupportNetworkList.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/SupportNetworkList.java
@@ -1,6 +1,8 @@
 package com.discoverandchange.pornographycrisissupport;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
@@ -29,6 +31,43 @@ public class SupportNetworkList extends BaseNavigationActivity {
     startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
   }
 
+  public void retrieveContactData(String cid) {
+
+    ContentResolver cr = getContentResolver();
+
+    Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+              ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + cid, null, null);
+      while (phones.moveToNext()) {
+        String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+        switch (type) {
+          case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+            // do something with the Home number here...
+
+            Log.v(Constants.LOG_TAG, "Home Phone Number: " + number);
+            Log.w(Constants.LOG_TAG, "Warning: Contact Home Phone Number not Handled Correctly");
+            break;
+          case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+            // do something with the Mobile number here...
+
+            Log.v(Constants.LOG_TAG, "Mobile Phone Number: " + number);
+            Log.w(Constants.LOG_TAG, "Warning: Contact Mobile Phone Number not Handled Correctly");
+            break;
+          case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+            // do something with the Work number here...
+
+            Log.v(Constants.LOG_TAG, "Work Phone Number: " + number);
+            Log.w(Constants.LOG_TAG, "Warning: Contact Work Phone Number not Handled Correctly");
+            break;
+        }
+
+        // TO DO :  Create a Support Contact with Name, with phone number and cid stored
+
+      }
+      phones.close();
+
+  }
+
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -41,6 +80,7 @@ public class SupportNetworkList extends BaseNavigationActivity {
           Log.v(Constants.LOG_TAG, "uri is " + result.toString());
 
           String id = result.getLastPathSegment();
+          retrieveContactData(id);
         }
           break;
       }

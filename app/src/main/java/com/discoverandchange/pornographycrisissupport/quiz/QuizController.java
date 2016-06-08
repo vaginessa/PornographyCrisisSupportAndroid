b235e2c75df7debcd7cc06ef9ce785218b69fafe
Stephen Nielson <stephen@nielson.org>
2016-06-07 19:33:31 -0600

    SMS Contact. Control flow changes.
    Added control flow from QuizController to LibraryController for implementing the SMS Contact.

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/QuizController.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/QuizController.java
@@ -1,10 +1,15 @@
 package com.discoverandchange.pornographycrisissupport.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
 
 import com.discoverandchange.pornographycrisissupport.BaseNavigationActivity;
 import com.discoverandchange.pornographycrisissupport.R;
+import com.discoverandchange.pornographycrisissupport.library.LibraryController;
 
 public class QuizController extends BaseNavigationActivity {
 
@@ -12,5 +17,17 @@ public class QuizController extends BaseNavigationActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz);
+  }
+
+  public void handleSubmitQuizClick(View btn) {
+    SeekBar slider = (SeekBar)findViewById(R.id.cravingsLevelSlider);
+    if (slider == null) {
+      throw new RuntimeException("Could not find slider.  Check id is named properly");
+    }
+
+    Log.d("PornAddictionSupport", "handleSubmitQuizClick() called with: " + "btn = [" + btn + "]" + " slider equal to " + slider.getProgress());
+
+    Intent intent = new Intent(getBaseContext(), LibraryController.class);
+    startActivity(intent);
   }
 }

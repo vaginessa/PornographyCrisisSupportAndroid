1dabdd3d5a5e37358cc9dd7ddd8a834762d261c5
Stephen Nielson <stephen@nielson.org>
2016-06-18 10:15:42 -0600

    Added QuizHistoryController
    Added the quiz history controller to the navigation and action bar.

---
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/QuizHistoryController.java
@@ -0,0 +1,16 @@
+package com.discoverandchange.pornographycrisissupport.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.discoverandchange.pornographycrisissupport.BaseNavigationActivity;
+import com.discoverandchange.pornographycrisissupport.R;
+
+public class QuizHistoryController extends BaseNavigationActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_quiz_history_controller);
+  }
+}

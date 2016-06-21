9ac5961d768a22f857a7ae284e2ef693d93f6cee
Stephen Nielson <stephen@nielson.org>
2016-06-21 15:58:43 -0600

    Debug tag and initial logs in resource library.

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/Constants.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/Constants.java
@@ -5,8 +5,10 @@ package com.discoverandchange.pornographycrisissupport;
  * Created by snielson on 6/7/16.
  */
 public class Constants {
-  public static final String LOG_TAG = "pornographysupportapp";
+  public static final String LOG_TAG = "psa";
   public static final String DATABASE_NAME = "pcs.db";
   public static final int DATABASE_VERSION = 1;
   public static final String SUPPORT_CONTACT_EDIT_MESSAGE = "supportNetwork.edit";
+
+  public static final String LOG_TAG_DEBUG  = "psa.debug";
 }
--- app/src/main/java/com/discoverandchange/pornographycrisissupport/library/ResourceLibraryService.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/library/ResourceLibraryService.java
@@ -1,5 +1,8 @@
 package com.discoverandchange.pornographycrisissupport.library;
 
+import android.util.Log;
+
+import com.discoverandchange.pornographycrisissupport.Constants;
 import com.discoverandchange.pornographycrisissupport.quiz.Quiz;
 
 import java.util.ArrayList;
@@ -9,10 +12,18 @@ import java.util.List;
  * Created by snielson on 6/6/16.
  */
 public class ResourceLibraryService {
+  public ResourceLibraryService() {
+    Log.i(Constants.LOG_TAG_DEBUG, "ResourceLibraryService constructor called.");
+  }
   public List<LibraryResource> getResourcesForQuiz(Quiz quiz) {
     return new ArrayList<LibraryResource>();
   }
 
   public void addResources(Range range, List<LibraryResource> mild) {
+    if (mild == null) {
+      String error = "attempt to add null resources in library service.";
+      Log.wtf(Constants.LOG_TAG, error);
+      return;
+    }
   }
 }

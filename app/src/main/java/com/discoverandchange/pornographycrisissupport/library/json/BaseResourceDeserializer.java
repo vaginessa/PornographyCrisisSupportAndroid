d11f77ca0d88fd9ca263db19a979fefb0fd34cc5
Stephen Nielson <stephen@nielson.org>
2016-06-27 14:48:39 -0600

    Updated java docs.

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/library/json/BaseResourceDeserializer.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/library/json/BaseResourceDeserializer.java
@@ -5,10 +5,18 @@ import com.discoverandchange.pornographycrisissupport.library.BaseResource;
 import org.json.JSONObject;
 
 /**
- * Created by snielson on 6/23/16.
+ * Abstract class that handles the conversion from a JSON object to all the filled in properties
+ * of a BaseResource class.
+ * @author Stephen Nielson
  */
 public abstract class BaseResourceDeserializer implements ResourceDeserializer {
 
+  /**
+   * This will copy all of the BaseResource properties that are in the primitive form in the
+   * JSONObject onto the BaseResource object provided.  Essentially hydrating the object.
+   * @param resource The resource that we want to be populated.
+   * @param jsonResource The javascript object notation values for this object.
+   */
   protected void hydrateBaseResource(BaseResource resource, JSONObject jsonResource) {
 
     String description = jsonResource.optString("description", "");

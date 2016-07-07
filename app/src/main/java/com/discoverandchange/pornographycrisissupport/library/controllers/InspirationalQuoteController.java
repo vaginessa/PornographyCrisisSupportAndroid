87c59a07c4b8aafd35e7464babae5fcf623dd65a
Stephen Nielson <stephen@nielson.org>
2016-07-07 15:20:43 -0600

    Implemented error todo in quote view.

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/library/controllers/InspirationalQuoteController.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/library/controllers/InspirationalQuoteController.java
@@ -3,6 +3,7 @@ package com.discoverandchange.pornographycrisissupport.library.controllers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.discoverandchange.pornographycrisissupport.Constants;
@@ -20,7 +21,9 @@ public class InspirationalQuoteController extends AppCompatActivity {
     Intent intent = this.getIntent();
     Object data = intent.getSerializableExtra(Constants.LIBRARY_RESOURCE_VIEW_MESSAGE);
     if (data == null || !(data instanceof InspirationalQuoteResource)) {
-      // TODO: stephen handle this error case.
+      Log.wtf(Constants.LOG_TAG, "Sent null inspirational quote resource when we shouldn't have");
+      finish();
+      return;
     }
     InspirationalQuoteResource resource = (InspirationalQuoteResource)data;
     txtInspirationalQuote.setText(resource.getText());

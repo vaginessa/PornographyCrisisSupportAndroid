3174b06a17b0428454681e573a081aac4f2180fe
Higbee-Keith <hig12012@byui.edu>
2016-07-07 20:12:27 -0700

    Working on framework for widget.  Not yet able to directly launch dialer.

---
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/CrisisAppWidget.java
@@ -0,0 +1,66 @@
+package com.discoverandchange.pornographycrisissupport;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.discoverandchange.pornographycrisissupport.quiz.QuizController;
+import com.discoverandchange.pornographycrisissupport.supportnetwork.SupportContact;
+import com.discoverandchange.pornographycrisissupport.supportnetwork.SupportNetworkService;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class CrisisAppWidget extends AppWidgetProvider {
+
+  @Override
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    // There may be multiple widgets active, so update all of them
+    for (int appWidgetId : appWidgetIds) {
+      updateAppWidget(context, appWidgetManager, appWidgetId);
+    }
+  }
+
+  @Override
+  public void onEnabled(Context context) {
+    // Enter relevant functionality for when the first widget is created
+    super.onEnabled(context);
+    //SupportContact supportContact;
+    //SupportNetworkService supportNetworkService = SupportNetworkService.getInstance(context);
+    //supportContact = supportNetworkService.getCrisisSupportContact();
+  }
+
+  @Override
+  public void onDisabled(Context context) {
+    // Enter relevant functionality for when the last widget is disabled
+  }
+
+  public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                              int appWidgetId) {
+
+    // Create an intent to launch the Dialer
+    QuizController quizController = new QuizController();
+    Intent intent = new Intent(context, QuizController.class);
+//    intent.putExtra("methodName", "myMethod");
+
+//    if (intent.getStringExtra("methodName").equals("myMethod")){
+//      quizController.launchDialer();
+//    }
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+    // Construct the RemoteViews object
+    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.crisis_app_widget);
+    //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+    // Get the layout and attach an on-click listener
+    views.setOnClickPendingIntent(R.id.widgetImage, pendingIntent);
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views);
+  }
+
+}
+

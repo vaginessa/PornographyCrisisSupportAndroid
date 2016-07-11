025e4b2f6a8711b4dba99ba5994248955b39f443
Stephen Nielson <stephen@nielson.org>
2016-07-11 10:39:29 -0600

    Moved private methods below protected/public

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/controllers/QuizController.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/quiz/controllers/QuizController.java
@@ -51,11 +51,9 @@ public class QuizController extends BaseNavigationActivity {
       QuizService service = new QuizService(getBaseContext());
       service.saveQuiz(new Quiz(10));
       handleCrisisQuizScore();
-    }
-    else if (isFirstUseSetupLaunch()) {
+    } else if (isFirstUseSetupLaunch()) {
       launchFirstUseSetup();
-    }
-    else {
+    } else {
       // gets back the data for the display
       String[] from = {ScoresTable.SCORE};
       int[] to = {android.R.id.text1};
@@ -63,6 +61,71 @@ public class QuizController extends BaseNavigationActivity {
           android.R.layout.simple_list_item_1, null, from, to, 0);
     }
   }
+
+  /**
+   * Handles the flow of saving a quiz score.  If the score is a crisis score, it
+   * launches the dialer.  Otherwise, it launches the library
+   *
+   * @param btn Used for determining debugging
+   */
+  public void handleSubmitQuizClick(View btn) {
+    SeekBar slider = (SeekBar) findViewById(R.id.cravingsLevelSlider);
+    if (slider == null) {
+      throw new RuntimeException("Could not find slider.  Check id is named properly");
+    }
+
+    Log.d("PornAddictionSupport", "handleSubmitQuizClick() called with: "
+        + "btn = [" + btn + "]" + " slider equal to " + slider.getProgress());
+
+    // save slider value to new Quiz
+    Quiz quiz = new Quiz();
+    // slider is 0 based so we have to add one.  Converts 0 -> 9 into 1 -> 10
+    quiz.setScore(slider.getProgress() + 1);
+
+    // Save the score (passes the current context to the QuizService)
+    boolean isCrisisQuizScore = (new QuizService(this)).saveQuiz(quiz);
+
+    // get the latest score
+    int latestScore = (new QuizService(this)).getLatestQuizScore();
+
+    if (isCrisisQuizScore) {
+      
+    } else {
+      launchLibrary();
+    }
+  }
+
+  /**
+   * Launch the resource library for people not in severe crisis mode.
+   */
+  public void launchLibrary() {
+    Intent intent = new Intent(getBaseContext(), LibraryController.class);
+    startActivity(intent);
+  }
+
+  /**
+   * Launches the dialer if the support network has been created.
+   */
+  public void launchDialer() {
+    SupportNetworkService service = SupportNetworkService.getInstance(getBaseContext());
+    SupportContact contact = service.getCrisisSupportContact();
+
+    if (contact != null) {
+      EndCallListener listener = new EndCallListener();
+      TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context
+          .TELEPHONY_SERVICE);
+      telephonyManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+
+      Uri call = Uri.parse("tel:" + contact.getPhoneNumber());
+      Intent intent = new Intent(Intent.ACTION_DIAL, call);
+      startActivity(intent);
+    } else {
+      // if we don't have a crisis contact then we should just launch the library
+      Log.d(Constants.LOG_TAG, "SupportContact crisis not found, launching library");
+      launchLibrary();
+    }
+  }
+
 
   /**
    * Launches the first use setup.
@@ -101,40 +164,6 @@ public class QuizController extends BaseNavigationActivity {
   }
 
 
-
-  /**
-   * Handles the flow of saving a quiz score.  If the score is a crisis score, it
-   * launches the dialer.  Otherwise, it launches the library
-   *
-   * @param btn Used for determining debugging
-   */
-  public void handleSubmitQuizClick(View btn) {
-    SeekBar slider = (SeekBar) findViewById(R.id.cravingsLevelSlider);
-    if (slider == null) {
-      throw new RuntimeException("Could not find slider.  Check id is named properly");
-    }
-
-    Log.d("PornAddictionSupport", "handleSubmitQuizClick() called with: "
-        + "btn = [" + btn + "]" + " slider equal to " + slider.getProgress());
-
-    // save slider value to new Quiz
-    Quiz quiz = new Quiz();
-    // slider is 0 based so we have to add one.  Converts 0 -> 9 into 1 -> 10
-    quiz.setScore(slider.getProgress() + 1);
-
-    // Save the score (passes the current context to the QuizService)
-    boolean isCrisisQuizScore = (new QuizService(this)).saveQuiz(quiz);
-
-    // get the latest score
-    int latestScore = (new QuizService(this)).getLatestQuizScore();
-
-    if (isCrisisQuizScore) {
-
-    } else {
-      launchLibrary();
-    }
-  }
-
   /**
    * Sends off text messages and launches the dialer when we have a crisis quiz score.
    */
@@ -150,37 +179,5 @@ public class QuizController extends BaseNavigationActivity {
    */
   private void sendSupportNetworkTexts() {
     SupportNetworkService.getInstance(getBaseContext()).contactNetwork();
-  }
-
-  /**
-   * Launch the resource library for people not in severe crisis mode.
-   */
-  public void launchLibrary() {
-    Intent intent = new Intent(getBaseContext(), LibraryController.class);
-    startActivity(intent);
-  }
-
-  /**
-   * Launches the dialer if the support network has been created.
-   */
-  public void launchDialer() {
-    SupportNetworkService service = SupportNetworkService.getInstance(getBaseContext());
-    SupportContact contact = service.getCrisisSupportContact();
-
-    if (contact != null) {
-      EndCallListener listener = new EndCallListener();
-      TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context
-          .TELEPHONY_SERVICE);
-      telephonyManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
-
-      Uri call = Uri.parse("tel:" + contact.getPhoneNumber());
-      Intent intent = new Intent(Intent.ACTION_DIAL, call);
-      startActivity(intent);
-    } else {
-      // if we don't have a crisis contact then we should just launch the library
-      Log.d(Constants.LOG_TAG, "SupportContact crisis not found, launching library");
-      launchLibrary();
-    }
-
   }
 }

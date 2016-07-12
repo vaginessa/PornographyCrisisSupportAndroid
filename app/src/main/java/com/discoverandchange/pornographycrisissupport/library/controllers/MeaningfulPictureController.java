681c730c04e4272597ed5cf1f8214b5526a7c4c5
Stephen Nielson <stephen@nielson.org>
2016-07-12 13:24:57 -0600

    Error display message if no picture found.

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/library/controllers/MeaningfulPictureController.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/library/controllers/MeaningfulPictureController.java
@@ -2,19 +2,27 @@ package com.discoverandchange.pornographycrisissupport.library.controllers;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.discoverandchange.pornographycrisissupport.BaseNavigationActivity;
 import com.discoverandchange.pornographycrisissupport.Constants;
 import com.discoverandchange.pornographycrisissupport.R;
 import com.discoverandchange.pornographycrisissupport.library.MeaningfulPictureResource;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.Text;
 
 import java.io.File;
 
+/**
+ *
+ */
 public class MeaningfulPictureController extends BaseNavigationActivity {
 
   @Override
@@ -32,7 +40,23 @@ public class MeaningfulPictureController extends BaseNavigationActivity {
 
     MeaningfulPictureResource resource = (MeaningfulPictureResource)obj;
     ImageView imageView = (ImageView)findViewById(R.id.imageMeaningfullPicture);
+    if (StringUtils.isEmpty(resource.getUrl())) {
+      Log.wtf(Constants.LOG_TAG, "sent invalid meaningful picture resource");
+    }
+
+    final TextView txtNoImage = (TextView)findViewById(R.id.txtMeaningfulPictureNoImage);
     Uri uri = Uri.parse(resource.getUrl());
-    Picasso.with(getBaseContext()).load(uri).into(imageView);
+    Picasso.with(getBaseContext()).load(uri).into(imageView,
+        new Callback() {
+          @Override
+          public void onSuccess() {
+            txtNoImage.setVisibility(View.INVISIBLE);
+          }
+
+          @Override
+          public void onError() {
+            txtNoImage.setVisibility(View.VISIBLE);
+          }
+        });
   }
 }

2fc464ea6655371b45bd173d92553fe2701d4e78
Stephen Nielson <stephen@nielson.org>
2016-07-15 15:59:05 -0600

    Fix javadoc for PicassoListener

--- app/src/main/java/com/discoverandchange/pornographycrisissupport/utils/PicassoListener.java
+++ app/src/main/java/com/discoverandchange/pornographycrisissupport/utils/PicassoListener.java
@@ -7,9 +7,16 @@ import com.discoverandchange.pornographycrisissupport.Constants;
 import com.squareup.picasso.Picasso;
 
 /**
- * Created by snielson on 7/8/16.
+ * Handles the logging and displaying of errors when a picasso image fails to load.
  */
 public class PicassoListener implements Picasso.Listener {
+
+  /**
+   * Logs that the image failed to load and the specific uri for debugging purposes.
+   * @param picasso {@inheritDoc}
+   * @param uri {@inheritDoc}
+   * @param exception {@inheritDoc}
+   */
   @Override
   public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
     Log.e(Constants.LOG_TAG, "Failed to load image for uri: " + uri.toString(), exception);
